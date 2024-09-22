9e15ddb6e762f2bbf90ac357a5687c856efdcaf9
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-09-22 21:00:23 +0900

    Update and rename 4text2.f90 to Chapter4/4text2.f90

---
+++ Chapter4/4text2.f90
@@ -0,0 +1,16 @@
+! p42 4-2
+! 4text2.f90
+! The latest version (2024/9/22)
+
+program practice
+implicit none
+
+	real,parameter::sng = 0.1e0         ! single precision
+	real(8),parameter::dbl = 0.1d0      ! double precision 
+	real(8)::double_sng
+ 
+	double_sng = sng                          ! Change single into double
+	write(*,*)"[from single to double] 0.1e0 = ", double_sng
+	write(*,*)"[double] 0.1d0 = ", dbl
+
+end program practice
