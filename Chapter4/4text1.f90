28e70e617ce622be2bb62be91ce82bd02124c190
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-09-22 21:02:42 +0900

    Update 4text1.f90

--- Chapter4/4text1.f90
+++ Chapter4/4text1.f90
@@ -9,15 +9,15 @@ implicit none
 	real::s
 
 	write(*,*)'Input integer t (< 86400000).'
-	read(*,*)t                                   ! ex) t = 80020531
+	read(*,*)t                                              ! ex) t = 80020531
 	if (t >=86400000) stop 't should be < 86400000.'
  
-	h = t / 3600000                                  ! h = 80020531 / 3600000 = 22 (because h is an integer)                             
-	m = (t - h * 3600000) / 60000                        ! m = 820531 / 60000 = 13 (because m is an integer)                           
-	s = (t - h * 3600000 - m * 60000) * 1.0e-3                ! s = 40531 * 1.0e-3 = 40.531
+	h = t / 3600000                                         ! h = 80020531 / 3600000 = 22 (because h is an integer)                             
+	m = (t - h * 3600000) / 60000                           ! m = 820531 / 60000 = 13 (because m is an integer)                           
+	s = (t - h * 3600000 - m * 60000) * 1.0e-3              ! s = 40531 * 1.0e-3 = 40.531
 
 	if (h > 11) then                        
-		write(*,*)'P.M.', (h - 12), ':' , m, ':', s         ! 22 - 12 = 10
+		write(*,*)'P.M.', (h - 12), ':' , m, ':', s     ! 22 - 12 = 10
 	else
  		write(*,*)'A.M.', h, ':', m, ':', s 
 	end if
