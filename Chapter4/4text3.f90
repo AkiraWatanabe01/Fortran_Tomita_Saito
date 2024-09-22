824ab20843f3f0163e8c89c811a1f34b0f5a940c
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-09-22 21:15:29 +0900

    Update and rename 4text3.f90 to Chapter4/4text3.f90

---
+++ Chapter4/4text3.f90
@@ -0,0 +1,23 @@
+! p42 4-3
+! 4text3.f90
+! The latest version (2024/9/22)
+
+program practice
+implicit none
+
+	integer::n 
+	real::single_precision_root
+	real(8)::double_precision_root, doubled_single_precision_root
+
+	write(*,*)'Input integer n.'
+	read(*,*)n
+
+	single_precision_root = sqrt(real(n))
+	double_precision_root = sqrt(real(n))
+	doubled_single_precision_root = single_precision_root ** 2            ! Change single into double
+
+	write(*,*)'single_precision_root = ', single_precision_root
+	write(*,*)'double_precision_root = ', double_precision_root
+	write(*,*)'doubled_single_precision_root = ', doubled_single_precision_root
+
+end program practice
