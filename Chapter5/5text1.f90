9a854f4f8840a2733203a1836f8e826d9685dbed
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-09-24 21:56:01 +0900

    Update and rename 5text1.f90 to Chapter5/5text1.f90

---
+++ Chapter5/5text1.f90
@@ -0,0 +1,26 @@
+! Page56 5-1
+! 5text1.f90
+! The latest version (2024/9/24)
+
+program practice
+implicit none
+
+	integer::budget, price      
+	integer::total_price = 0	! At first, set "total_price = 0" to use "do while" command.
+
+	! Set budget
+	write(*,*)"Input your budget."
+	read(*,*)budget
+
+	! Repeat reading prices
+	do while (total_price <= budget)
+		! If "total_price" is smaller than "budget", "do while" continues.
+		write(*,*)"Input the price."
+		read(*,*)price
+		total_price = total_price + price
+	end do
+
+	! Display the total price
+	write(*,*)"The total price : ", total_price
+
+end program practice
