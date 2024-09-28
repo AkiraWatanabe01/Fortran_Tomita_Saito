54034601c68ac9b66d0fab4bf413c840decd663b
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-09-28 22:53:41 +0900

    Update exercise 5_5 code

--- Chapter5/5text5.f90
+++ Chapter5/5text5.f90
@@ -6,7 +6,7 @@ program practice
 implicit none
  
 	integer::k, m, n
-	real::S, x, h
+	real::S, x, h        ! Area, x, increment
 
 	write(*,*)"The ratio of the circumference of a circle and its diameter"
 
@@ -14,18 +14,18 @@ implicit none
 		n = 2 ** k
 		h = 1.0e0 / n
 
-		! The initial value of S
-        	! S = h*0.5e0*f(x_0)+h*0.5e0*f(x_n) 
-                !   = h*0.5e0*(f(0)+f(1))
-                !   = h*3.0e0   
-		S=h*3.0e0
+                ! The initial value of S
+                ! S = 0.5e0 * f(x_0) + h * 0.5e0 *f(x_n) 
+                !   = 0.5e0 * (f(0) + f(1))
+                !   = 3.0e0   
+		S = 3.0e0
 
 		do m = 1, (n - 1)
 			x = h * m
 			S = S + 4.0e0 / (1.0e0 + x ** 2.0e0)
 		end do
-  		S = S * h
-		write(*,'(1x, 3I, 1x, f8.6)')k, S
+		S = S * h
+		write(*,'(1x, i3, 1x, f10.8)')k, S
 	end do
 
 end program practice
