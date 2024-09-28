48c8383d374111a1ea930d5fb06d846753eb433b
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-09-28 10:00:07 +0900

    Update and rename 5text4.f90 to Chapter5/5text4.f90

---
+++ Chapter5/5text4.f90
@@ -0,0 +1,33 @@
+! Page56 5-4 
+! 5text4.f90
+! The latest version (2024/9/28)
+
+program practice
+implicit none
+
+	integer::j, k, n, m
+
+ 	! Upper part
+	! n=1        * 
+	! n=2       ***
+        ! n=3      ***** 
+        ! n=4     *******
+        ! n=5    ********* 
+        ! n=6   ***********
+        ! n=7  *************
+	do n = 1, 7
+		write(*,*)(" ", j = 1, 7 - n), ("*", k = 1, 2 * n - 1)         
+	end do                                 
+
+	! Lower part
+ 	! m=1   ***********
+	! m=2    *********
+        ! m=3     *******
+        ! m=4      *****  
+        ! m=5       ***
+        ! m=6        *
+	do m = 1, 6                                     
+		write(*,*)(" ", j = 1, m), ("*", k = 1, -2 * m + 13)         
+	end do
+
+end program practice                     
