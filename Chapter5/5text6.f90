a55bfe45ec348b4a1d7a758ea26039e88fc45d7f
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-09-30 06:37:57 +0900

    Update and rename 5text6.f90 to Chapter5/5text6.f90

---
+++ Chapter5/5text6.f90
@@ -0,0 +1,37 @@
+! Page57 5-6
+! 5text6.f90
+! The latest version (2024/9/30)
+
+program practice
+implicit none
+
+	integer::x, y, z, k, m, n
+	real::z0, v
+
+	! x should be smaller than 71 
+	do x = 1, 70
+		do y = (x + 1), 99
+			z0 = sqrt(real(x) ** 2.0E0 + real(y) **  2.0E0)
+			if (z0 > 100) cycle
+   
+			v = z0 - int(z0)
+			if (v /= 0) cycle
+
+			! Euclidean algorithm
+			m = x
+			n = y
+			do
+				k = mod(m, n)
+				if (k == 0) exit
+				m = n
+				n = k
+			end do
+
+			! Display the Pythagoras number 
+			if (n /= 1) cycle
+			z = int(z0)
+			write(*,*)x, y, z
+		end do
+	end do
+
+end program practice
