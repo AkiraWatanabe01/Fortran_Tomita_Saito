a49360110299a015e6998a9b9317be43fa1d8256
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-10-02 20:11:33 +0900

    Update and rename 5text8.f90 to Chapter5/5text8.f90

---
+++ Chapter5/5text8.f90
@@ -0,0 +1,27 @@
+! Page56 5-8
+! 5text8.f90
+! The latest version (2024/10/2)
+
+program practice
+implicit none
+ 
+	integer::i
+ 	integer::num, sum
+
+	do
+		write(*,*)"Input a positive integer or 0 to stop :"
+		read(*,*)num
+  		if (num < 0) stop "Input a POSITIVE integer."
+		if (num == 0) exit
+
+		! The sum of divisors
+		sum = 1 + num
+		do i = 2, num - 1
+			if (mod(num, i) /= 0) cycle
+			sum = sum + i
+		end do
+
+		write(*,*)'The sum of divisors of ', num, 'is ', sum
+	end do
+
+end program practice
