04936075a42d53056ac34ffa261a2cbf96a114cf
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-10-02 20:04:43 +0900

    Update exercise 5_7 code

---
+++ Chapter5/5text7.f90
@@ -0,0 +1,31 @@
+! Page57 5-7
+! 5text7.f90
+! The latest version (2024/10/2)
+
+program practice
+implicit none
+
+	integer::n, m
+	integer::num, remainder, counter
+
+	write(*,*)'Input a positive integer.'
+	read(*,*)num
+
+	if (num == 1) then
+		write(*,*)1
+	end if
+
+	do n = 2, num
+		remainder = mod(num, n)
+		if (remainder /= 0) cycle
+		counter = 0
+
+		do while (mod(num, n) == 0)
+			num = num / n
+			counter = counter + 1
+		end do
+
+		write(*,*)(n, m = 1, counter)
+	end do
+
+end program practice
