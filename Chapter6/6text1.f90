8d462b2ca71257d85ced2158095604f1522b6de0
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-10-04 23:03:32 +0900

    Update and rename 6text1.f90 to Chapter6/6text1.f90

---
+++ Chapter6/6text1.f90
@@ -0,0 +1,41 @@
+! Page 85 6-1
+! 6text1.f90
+! The latest version (2024/10/4)
+
+program practice
+implicit none
+
+	integer::i, j, k
+	integer::timestep
+	integer, dimension(-100:100)::a_scale
+	real, dimension(-100:100)::a, a_minus, a_plus
+	real::dt = 1.0e0/16.0e0
+
+	! The initial condition
+	a(:) = (/(0.0e0, i = -100, 100)/)
+	a(0) = 10000.0e0
+
+	write(*,*)'How many steps do you have ?'
+	read(*,*)timestep
+
+	do j = 1, timestep
+		a_minus(:) = cshift(a(:), -1)
+		a_plus(:) = cshift(a(:), 1)
+
+		! Boundary condition
+  		a_minus(-100) = 0
+		a_minus(100) = 0
+		a_plus(-100) = 0
+		a_plus(100) = 0
+
+		! Update
+		a(:) = (1.0e0 - 2.0e0 * dt) * a(:) + dt * (a_minus(:) + a_plus(:))
+
+		! Drawing histograms to see the process (This is an original way.)   
+		do k = -100, 100
+			a_scale(k) = int(a(k)) / 100
+			write(*,'(1X, I4, "|", 100A1)')k, ('*', i = 1, a_scale(k))
+		end do
+	end do
+
+end program practice
