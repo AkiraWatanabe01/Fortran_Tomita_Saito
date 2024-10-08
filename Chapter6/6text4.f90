9556d7ce305ddd52b9b996afbf46fd97ab8d1380
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-10-08 21:38:27 +0900

    Update exercise 6_4 code

---
+++ Chapter6/6text4.f90
@@ -0,0 +1,25 @@
+! Page 85 6-4
+! 6text4.f90
+! The latest version (2024/10/8)
+
+program practice
+implicit none
+
+	integer::i
+	integer::money
+	integer, dimension(1:10)::paper = 0
+	integer, dimension(1:10)::currency = (/ 10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1/)
+
+	write(*,*)"Input money :"
+	read(*,*)money
+	if (money < 0) stop "It should be non-negative."
+
+	do i = 1, 10
+		do while (money >= currency(i))
+			paper(i) = paper(i) + 1
+			money = money - currency(i)
+		end do
+		write(*, '(I5, A5, 1X, I4)')currency(i), " yen:", paper(i)
+	end do
+
+end program practice
