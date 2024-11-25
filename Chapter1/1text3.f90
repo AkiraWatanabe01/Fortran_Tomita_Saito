099f7799e5acf3f8b80c50be2bd62c9880042761
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-11-25 21:32:34 +0900

    Update 1text3.f90

--- Chapter1/1text3.f90
+++ Chapter1/1text3.f90
@@ -1,22 +1,22 @@
 ! page8 1-3
 ! 1text3.f90
-! The latest version (2024/9/16)
+! The latest version (2024/11/25)
 
 program practice
-implicit none
 
-	real::w, h, BMI    ! weight, height, BMI               
+	implicit none
+	real :: w, h, BMI    ! weight, height, BMI               
 
-	write(*,*)'Input your weight[kg].' 
+	write(*,*)"Input your weight[kg]." 
 	read(*,*)w
 
-	write(*,*)'Input your height[m].'
+	write(*,*)"Input your height[m]."
 	read(*,*)h
 
 	! Calculation  
 	! BMI=w/h**2.0e0 is also OK.
 	BMI = w / (h * h)
 
-	write(*,*)'Your BMI is ',BMI     
+	write(*,*)"Your BMI is ", BMI     
 
 end program practice
