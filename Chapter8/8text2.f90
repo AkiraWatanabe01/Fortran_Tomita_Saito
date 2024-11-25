df61751f1ab93ec95f582590bfdd146a0a5428c8
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-11-25 20:32:43 +0900

    Update 8text2.f90

--- Chapter8/8text2.f90
+++ Chapter8/8text2.f90
@@ -4,10 +4,10 @@
 
 ! Subroutine
 subroutine cross_product(x, y, r)
-implicit none
 
-	real, dimension(1:3), intent(in)::x, y
-	real, dimension(1:3), intent(out)::z
+	implicit none
+	real, dimension(1:3), intent(in) :: x, y
+	real, dimension(1:3), intent(out) :: z
 
 	z(1) = x(2) * y(3) - x(3) * y(2)
 	z(2) = x(3) * y(1) - x(1) * y(3)
@@ -17,10 +17,10 @@ end subroutine cross_product
 
 ! Function
 function func_cross(x,y) result (z)
-implicit none
 
-	real, dimension(1:3), intent(in)::x, y
-	real, dimension(1:3)::z                     ! 'intent' is not necessary.
+	implicit none
+	real, dimension(1:3), intent(in) :: x, y
+	real, dimension(1:3) :: z                     ! 'intent' is not necessary.
 
 	z(1) = x(2) * y(3) - x(3) * y(2)
 	z(2) = x(3) * y(1) - x(1) * y(3)
@@ -30,11 +30,11 @@ end function func_cross
 
 ! Main
 program practice
-implicit none
 
-	real, dimension(1:3)::a, b, sub_cross
+	implicit none
+	real, dimension(1:3) :: a, b, sub_cross
 
-	interface                                  ! To use the array
+	interface                                     ! To use the array
  		function func_cross(x, y) result(z)
 			real, dimension(1:3),intent(in)::x, y
 			real, dimension(1:3)::z
