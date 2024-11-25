b9c6036c56ed2b3183c355c8ceb4d2abb5bcc675
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-11-25 21:30:41 +0900

    Update 1text2.f90

--- Chapter1/1text2.f90
+++ Chapter1/1text2.f90
@@ -1,19 +1,19 @@
 ! page8 1-2
 ! 1text2.f90
-! The latest version (2024/9/16)
+! The latest version (2024/11/25)
 
 program practice
-implicit none
 
-	real::C,F               
+	implicit none
+	real :: C, F               
 
-	write(*,*)'Input Celsius degree.' 
+	write(*,*)"Input Celsius degree." 
 	read(*,*)C     
 
 	! Calculation  
 	! Just 1.8 is OK, but 1.8e0 is better than 1.8. The same is true for 32e0.
 	F = 1.8 * C + 32.0 
 
-	write(*,*)'Fahrenheit : ',F     
+	write(*,*)"Fahrenheit : ", F     
 
 end program practice
