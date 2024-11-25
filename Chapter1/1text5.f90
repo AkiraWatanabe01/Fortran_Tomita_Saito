1e9a6cc72e8a890ca8c4dff73fb63dabd0870b0f
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-11-25 21:34:32 +0900

    Update 1text5.f90

--- Chapter1/1text5.f90
+++ Chapter1/1text5.f90
@@ -1,21 +1,21 @@
 ! page8 1-5
 ! 1text5.f90
-! The latest version (2024/9/16) 
+! The latest version (2024/11/25) 
 
 program practice
-implicit none
 
-	real::L, T, pi, g
+	implicit none
+	real :: L, T, pi, g
 
-	pi=3.1415926       ! Define pi (3.1415926e0)
-	g=9.8              ! Define g  (9.8e0)
+	pi = 3.1415926       ! Define pi (3.1415926e0)
+	g = 9.8              ! Define g  (9.8e0)
 
-	write(*,*)'Input the length of the string [m].'
+	write(*,*)"Input the length of the string [m]."
 	read(*,*)L
 
 	! Calculation
 	T = 2 * pi * (L / g) ** 0.5    ! 0.5e0 is better.
 
-	write(*,*)'The period is',T,'s'   ! 'words',variable,'words'
+	write(*,*)"The period is", T, "s"   ! "words", variable, "words"
 
 end program practice
