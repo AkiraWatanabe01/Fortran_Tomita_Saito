4242a7f2502d4403d28662e2d8ee0d6c4090b3e1
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-12-22 18:32:31 +0900

    Update 3text2.f90

--- Chapter3/3text2.f90
+++ Chapter3/3text2.f90
@@ -1,28 +1,28 @@
 ! p28 3-2
 ! 3text2.f90
-! The latest version (2024/9/20)
+! The latest version (2024/12/22)
 
 program practice
-implicit none
 
-	real::height, weight, BMI
+	implicit none
+	real :: height, weight, BMI
 
-	write(*,*)'What is your height [m]?'
+	write(*,*)"What is your height [m]?"
 	read(*,*)height
  
-	write(*,*)'What is your weight [kg]?'
+	write(*,*)"What is your weight [kg]?"
 	read(*,*)weight
 
 	! Calculation
 	BMI = weight / (height * height)
 
-	write(*,*)'Your BMI is', BMI, '.'
+	write(*,*)"Your BMI is", BMI, "."
 	if (BMI <= 18.5e0) then
-		write(*,*)'So, you are thin.'
+		write(*,*)"So, you are thin."
 	else if ((BMI > 18.5e0).and.(BMI < 25e0)) then 
-		write(*,*)'So, you are normal.'
+		write(*,*)"So, you are normal."
 	else
-		write(*,*)'So, you are overweight.'
+		write(*,*)"So, you are overweight."
 	end if
 
 end program practice
