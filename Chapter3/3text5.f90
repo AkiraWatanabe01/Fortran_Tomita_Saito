a42d49a35d56fc960ad5b1be26b593e6c463a7c9
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-12-22 18:40:54 +0900

    Update 3text5.f90

--- Chapter3/3text5.f90
+++ Chapter3/3text5.f90
@@ -1,17 +1,17 @@
 ! p28 3-5
 ! 3text5.f90
-! The latest version (2024/9/21)
+! The latest version (2024/12/22)
 
 program practice
-implicit none
- 
-	real::x, decimal_part
 
-	write(*,*)'Input real number x'
+	implicit none
+	real :: x, decimal_part
+
+	write(*,*)"Input real number x"
  	read(*,*)x
 
 	decimal_part = abs(x) - aint(abs(x))
-	if ((x < 0e0).and.(decimal_part /= 0e0)) then
+	if ((x < 0e0) .and. (decimal_part /= 0e0)) then
 		decimal_part = 1e0 - decimal_part
 	end if
 
