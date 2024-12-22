0e82d58c44eb2f48e20c84b030d2174318b2cea2
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-12-22 18:42:48 +0900

    Update 3text6.f90

--- Chapter3/3text6.f90
+++ Chapter3/3text6.f90
@@ -1,26 +1,26 @@
 ! p28 3-6
 ! 3text6.f90
-! The latest version (2024/9/21)
+! The latest version (2024/12/22)
 
 program practice
-implicit none
- 
-	integer::i1, j1, k1, i2, j2, k2, t1, t2, dif, i3, j3, k3
 
-	write(*,*)'Input i1 hour, j1 minute, and k1 second'
-	write(*,*)'i1 ?'
+	implicit none
+	integer :: i1, j1, k1, i2, j2, k2, t1, t2, dif, i3, j3, k3
+
+	write(*,*)"Input i1 hour, j1 minute, and k1 second"
+	write(*,*)"i1 ?"
 	read(*,*)i1
-	write(*,*)'j1 ?'
+	write(*,*)"j1 ?"
 	read(*,*)j1
-	write(*,*)'k1 ?'
+	write(*,*)"k1 ?"
 	read(*,*)k1
 
-	write(*,*)'Input i2 hour, j2 minute, and k2 second'
-	write(*,*)'i2 ?'
+	write(*,*)"Input i2 hour, j2 minute, and k2 second"
+	write(*,*)"i2 ?"
 	read(*,*)i2
-	write(*,*)'j2 ?'
+	write(*,*)"j2 ?"
 	read(*,*)j2
-	write(*,*)'k2 ?'
+	write(*,*)"k2 ?"
 	read(*,*)k2
 
 	! Convert hour and minute into second
@@ -33,6 +33,6 @@ implicit none
 	j3 = (dif - i3 * 3600) / 60
 	k3 = dif - i3 * 3600 - j3 * 60
 
-	write(*,*)i3, 'hour', j3, 'minute', k3, 'second'
+	write(*,*)i3, "hour", j3, "minute", k3, "second"
 
 end program practice
