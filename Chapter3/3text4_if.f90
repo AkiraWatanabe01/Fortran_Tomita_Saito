88d7c3c20a6ed0289fcb4962f35139e595a978e6
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-12-22 18:39:48 +0900

    Update 3text4_if.f90

--- Chapter3/3text4_if.f90
+++ Chapter3/3text4_if.f90
@@ -1,32 +1,32 @@
 ! p28 3-4
 ! 3text4_if.f90
-! The latest version (2024/9/21)
+! The latest version (2024/12/22)
 
 program practice
-implicit none
 
-	integer::n, remainder
+	implicit none
+	integer :: n, remainder
 
-	write(*,*)'Input integer n: '
+	write(*,*)"Input integer n: "
 	read(*,*)n
  
 	remainder = mod(n, 7)
  
 	! IF statement (SELECT CASE statement is better in this exercise!) 
 	if (remainder == 0) then          
-		write(*,*)'Sunday'
+		write(*,*)"Sunday"
 	else if (remainder == 1) then 
-		write(*,*)'Monday'
+		write(*,*)"Monday"
 	else if (remainder == 2) then 
-		write(*,*)'Tuesday'
+		write(*,*)"Tuesday"
 	else if (remainder == 3) then 
-		write(*,*)'Wednesday'
+		write(*,*)"Wednesday"
 	else if (remainder == 4) then 
-		write(*,*)'Thursday'
+		write(*,*)"Thursday"
 	else if (remainder == 5) then 
-		write(*,*)'Friday'
+		write(*,*)"Friday"
 	else  
-		write(*,*)'Saturday'
+		write(*,*)"Saturday"
 	end if
 
 end program practice
