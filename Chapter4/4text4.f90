1d61680275e1b1fd2254c720186ce17136ab59e6
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-12-31 21:54:31 +0900

    Update 4text4.f90

--- Chapter4/4text4.f90
+++ Chapter4/4text4.f90
@@ -1,26 +1,26 @@
 ! p42 4-4
 ! 4text4.f90
-! The latest version (2024/9/22)
+! The latest version (2024/12/31)
 
 program practice
-implicit none
- 
-	real::x, y        
-	integer::z
 
-	write(*,*)'Input real number x.'
+	implicit none
+	real :: x, y        
+	integer :: z
+
+	write(*,*)"Input real number x."
 	read(*,*)x
 
 	z = int(x)
 	y = x - z
 
 	if (y == 0e0) then
-		write(*,*)'f = c =', z
+		write(*,*)"f = c =", z
 	else
 		if (x >= 0e0) then
-			write(*,*)'f =', z, ', c =', z + 1
+			write(*,*)"f =", z, ", c =", z + 1
 		else
-			write(*,*)'f =', z - 1, ', c =', z
+			write(*,*)"f =", z - 1, ", c =", z
 		end if
 	end if
 
