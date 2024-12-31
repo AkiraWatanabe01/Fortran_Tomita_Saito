1d48021231e71aa5fb1660d0a4e84b71617f9df8
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2024-12-31 21:45:29 +0900

    Update 4text2.f90

--- Chapter4/4text2.f90
+++ Chapter4/4text2.f90
@@ -1,15 +1,15 @@
 ! p42 4-2
 ! 4text2.f90
-! The latest version (2024/9/22)
+! The latest version (2024/12/31)
 
 program practice
-implicit none
 
-	real,parameter::sng = 0.1e0         ! single precision
-	real(8),parameter::dbl = 0.1d0      ! double precision 
-	real(8)::double_sng
+	implicit none
+	real, parameter :: sng = 0.1e0                            ! single precision
+	real(8), parameter :: dbl = 0.1d0                         ! double precision 
+	real(8) :: double_sng
  
-	double_sng = sng                          ! Change single into double
+	double_sng = sng                                          ! Change single into double
 	write(*,*)"[from single to double] 0.1e0 = ", double_sng
 	write(*,*)"[double               ] 0.1d0 = ", dbl
 
