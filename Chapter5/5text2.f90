b82bf749144b8cecd5b48b234fbb02d539dd3d6a
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2025-01-04 21:04:16 +0900

    Update 5text2.f90

--- Chapter5/5text2.f90
+++ Chapter5/5text2.f90
@@ -1,13 +1,13 @@
 ! Page56 5-2
 ! 5text2.f90
-! The latest version (2024/9/26)
+! The latest version (2025/1/4)
 
 program practice
-implicit none
 
-	integer::n
-	integer::x_n, x_one = 1, x_two = 1	! x_one = x_{n-1}, x_two = x_{n-2}
-	real(8)::ratio				! The ratio of x_n and x_n-1
+	implicit none
+	integer :: n
+	integer :: x_n, x_one = 1, x_two = 1		! x_one = x_{n-1}, x_two = x_{n-2}
+	real(8) :: ratio				! The ratio of x_n and x_n-1
 
 	do n = 2, 45
  		! Fibonacci's sequence
@@ -21,7 +21,7 @@ implicit none
 		x_one = x_n
 
 		! 2 integers, 1 blank, 10 integers, 1 blank, and 18 real numbers(16th decimal place)
-		write(*,'(i2, 1x, i10, 1x, f18.16)')n, x_n, ratio
+		write(*, "(I2, 1X, I10, 1X, F18.16)")n, x_n, ratio
 	end do
 
 end program practice
