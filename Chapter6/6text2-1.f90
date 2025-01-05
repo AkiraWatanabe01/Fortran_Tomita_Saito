4239a06e9bb7cccb5fdb7e67d39445d6471633a2
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2025-01-05 17:22:35 +0900

    Update 6text2-1.f90

--- Chapter6/6text2-1.f90
+++ Chapter6/6text2-1.f90
@@ -1,25 +1,25 @@
 ! Page 85 6-2(1)
 ! 6text2-1.f90
-! The latest version (2024/10/4)
+! The latest version (2025/1/5)
 
 program practice
-implicit none
 
-	integer::i, j, p, q
-	integer, dimension(1:9, 1:9)::original_matrix     ! original matrix
-	integer, dimension(1:8, 1:8)::b, c, d, e          ! partial matrix 
-	integer, dimension(1:8, 1:8)::final_matrix        ! final matrix
+	implicit none
+	integer :: i, j, p, q
+	integer, dimension(1:9, 1:9) :: original_matrix     ! original matrix
+	integer, dimension(1:8, 1:8) :: b, c, d, e          ! partial matrix 
+	integer, dimension(1:8, 1:8) :: final_matrix        ! final matrix
 
 	! Original matrix        
-	original_matrix(1, 1:9)=(/1, 2, 3, 4, 5, 6, 7, 8, 9/)
-	original_matrix(2, 1:9)=(/2, 2, 3, 4, 5, 6, 7, 8, 9/)
-	original_matrix(3, 1:9)=(/3, 3, 3, 4, 5, 6, 7, 8, 9/)
-	original_matrix(4, 1:9)=(/4, 4, 4, 4, 5, 6, 7, 8, 9/)
-	original_matrix(5, 1:9)=(/5, 5, 5, 5, 5, 6, 7, 8, 9/)
-	original_matrix(6, 1:9)=(/6, 6, 6, 6, 6, 6, 7, 8, 9/)
-	original_matrix(7, 1:9)=(/7, 7, 7, 7, 7, 7, 7, 8, 9/)
- 	original_matrix(8, 1:9)=(/8, 8, 8, 8, 8, 8, 8, 8, 9/)
-	original_matrix(9, 1:9)=(/9, 9, 9, 9, 9, 9, 9, 9, 9/)
+	original_matrix(1, 1:9) = (/1, 2, 3, 4, 5, 6, 7, 8, 9/)
+	original_matrix(2, 1:9) = (/2, 2, 3, 4, 5, 6, 7, 8, 9/)
+	original_matrix(3, 1:9) = (/3, 3, 3, 4, 5, 6, 7, 8, 9/)
+	original_matrix(4, 1:9) = (/4, 4, 4, 4, 5, 6, 7, 8, 9/)
+	original_matrix(5, 1:9) = (/5, 5, 5, 5, 5, 6, 7, 8, 9/)
+	original_matrix(6, 1:9) = (/6, 6, 6, 6, 6, 6, 7, 8, 9/)
+	original_matrix(7, 1:9) = (/7, 7, 7, 7, 7, 7, 7, 8, 9/)
+ 	original_matrix(8, 1:9) = (/8, 8, 8, 8, 8, 8, 8, 8, 9/)
+	original_matrix(9, 1:9) = (/9, 9, 9, 9, 9, 9, 9, 9, 9/)
 
 	b(:, :) = 0
 	c(:, :) = 0
@@ -40,17 +40,17 @@ implicit none
 	do
 		write(*,*)"Input j :"
 		read(*,*)j
-		if ((1 <= j).and.(j <= 9)) exit
+		if ((1 <= j) .and. (j <= 9)) exit
 		write(*,*)"Input an integer (1 to 9)"
 	end do
 
-	if ((i == 1).and.(j == 1)) then
+	if ((i == 1) .and. (j == 1)) then
 		final_matrix(1:8, 1:8) = original_matrix(2:9, 2:9)
-	else if ((i == 1).and.(j == 9)) then
+	else if ((i == 1) .and. (j == 9)) then
 		final_matrix(1:8, 1:8) = original_matrix(2:9, 1:8)
-	else if ((i == 9).and.(j == 1)) then
+	else if ((i == 9) .and. (j == 1)) then
 		final_matrix(1:8, 1:8) = original_matrix(1:8, 2:9)
-	else if ((i == 9).and.(j == 9)) then
+	else if ((i == 9) .and. (j == 9)) then
 		final_matrix(1:8, 1:8) = original_matrix(1:8, 1:8)
  	else
 		b(1:(i - 1), 1:(j - 1)) = original_matrix(1:(i - 1), 1:(j - 1))
