c20c42d066042b0dea35fe8f1b173297c326873a
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2025-01-05 17:19:05 +0900

    Update 6text1.f90

--- Chapter6/6text1.f90
+++ Chapter6/6text1.f90
@@ -1,21 +1,21 @@
 ! Page 85 6-1
 ! 6text1.f90
-! The latest version (2024/10/4)
+! The latest version (2025/1/5)
 
 program practice
-implicit none
 
-	integer::i, j, k
-	integer::timestep
-	integer, dimension(-100:100)::a_scale
-	real, dimension(-100:100)::a, a_minus, a_plus
-	real::dt = 1.0e0/16.0e0
+	implicit none
+	integer :: i, j, k
+	integer :: timestep
+	integer, dimension(-100:100) :: a_scale
+	real, dimension(-100:100) :: a, a_minus, a_plus
+	real :: dt = 1.0e0 / 16.0e0
 
 	! The initial condition
 	a(:) = (/(0.0e0, i = -100, 100)/)
 	a(0) = 10000.0e0
 
-	write(*,*)'How many steps do you have ?'
+	write(*,*)"How many steps do you have ?"
 	read(*,*)timestep
 
 	do j = 1, timestep
@@ -34,7 +34,7 @@ implicit none
 		! Drawing histograms to see the process (This is an original way.)   
 		do k = -100, 100
 			a_scale(k) = int(a(k)) / 100
-			write(*,'(1X, I4, "|", 100A1)')k, ('*', i = 1, a_scale(k))
+			write(*, '(1X, I4, "|", 100A1)')k, ('*', i = 1, a_scale(k))
 		end do
 	end do
 
