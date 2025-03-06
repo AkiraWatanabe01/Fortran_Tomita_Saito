a177275acc0df115ce0aa99ec9ee90c07311db66
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2025-03-06 21:47:25 +0900

    Update 7text8.f90

--- Chapter7/7text8.f90
+++ Chapter7/7text8.f90
@@ -1,20 +1,20 @@
 ! Page 106 7-8
 ! 7text8.f90
-! The latest version (2024/10/15)
+! The latest version (2025/3/6)
 
 program practice
-implicit none
 
-	character(len = 1)::alphabet
-	character(len = 7)::buf_alphabet(1:182)
-	integer::i
-	integer::row_top, row_bottom
-	integer, parameter::height_alphabet = 7
+	implicit none
+	character(len = 1) :: alphabet
+	character(len = 7) :: buf_alphabet(1:182)
+	integer :: i
+	integer :: row_top, row_bottom
+	integer, parameter :: height_alphabet = 7
 
 	do 
-		write(*, '(A)', advance='no')"Input a lower case alphabetical character :"
+		write(*, '(A)', advance = 'no')"Input a lower case alphabetical character :"
 		read(*,*)alphabet
-		if ((ichar('a') <= ichar(alphabet)).and.(ichar(alphabet) <= ichar('z'))) exit
+		if ((ichar('a') <= ichar(alphabet)) .and. (ichar(alphabet) <= ichar('z'))) exit
 	end do
 
 	row_top = height_alphabet * (ichar(alphabet) - ichar('a')) + 1
