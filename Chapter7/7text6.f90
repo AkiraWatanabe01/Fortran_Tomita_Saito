ed154c9b994de7e70a4e8b12bbecbec0c9625e0d
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2025-03-06 21:35:40 +0900

    Update 7text6.f90

--- Chapter7/7text6.f90
+++ Chapter7/7text6.f90
@@ -1,19 +1,19 @@
 ! Page 105 7-6  
 ! 7text6.f90
-! The latest version(2024/10/13)
+! The latest version(2025/3/6)
 
 program main
-implicit none
 
-	character(len = 100)::fmt
-	character(len = 2)::interval_odd(1:6) = (/'34', '28', '22', '16', '10', ' 4'/)
-	character(len = 2)::interval_even(1:6) = (/'31', '25', '19', '13', ' 7', ' 1'/)
-	character(len = 2)::repeat_odd(1:6)=(/' 1', ' 3', ' 5', ' 7', ' 9', '11'/)
-	character(len = 2)::repeat_even(1:6)=(/' 2', ' 4', ' 6', ' 8', '10', '12'/)
-	integer::i
-	integer::index
-	integer, parameter::num_max = 12
-	integer, allocatable::c(:)
+	implicit none
+	character(len = 100) :: fmt
+	character(len = 2) :: interval_odd(1:6) = (/'34', '28', '22', '16', '10', ' 4'/)
+	character(len = 2) :: interval_even(1:6) = (/'31', '25', '19', '13', ' 7', ' 1'/)
+	character(len = 2) :: repeat_odd(1:6) = (/' 1', ' 3', ' 5', ' 7', ' 9', '11'/)
+	character(len = 2) :: repeat_even(1:6) = (/' 2', ' 4', ' 6', ' 8', '10', '12'/)
+	integer :: i
+	integer :: index
+	integer, parameter :: num_max = 12
+	integer, allocatable :: c(:)
 
 	allocate(c(0:num_max))
 	c(:) = 0
