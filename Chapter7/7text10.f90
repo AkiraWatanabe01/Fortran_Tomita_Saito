57db9fb7249fd5f9a3d696b47c5cb300c97b6fda
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2025-03-06 21:55:20 +0900

    Update 7text10.f90

--- Chapter7/7text10.f90
+++ Chapter7/7text10.f90
@@ -1,23 +1,23 @@
 ! Page 106 7-10
 ! 7text10.f90
-! The latest version (2024/10/2)
+! The latest version (2025/3/6)
 
 program practice
-implicit none
 
-	integer::j
-	integer::num, composite
-	integer::index = 0
-	integer::factor(1:9), exponent(1:9)
-	integer::digit_factor(1:9), digit_exponent(1:9)
-	character(len = 80)::form
-	character(len = 2)::char_num(1:10)=(/"1 ", "2 ", "3 ", "4 ", "5 ", &
-					     "6 ", "7 ", "8 ", "9 ", "10"/)
+	implicit none
+	integer :: j
+	integer :: num, composite
+	integer :: index = 0
+	integer :: factor(1:9), exponent(1:9)
+	integer :: digit_factor(1:9), digit_exponent(1:9)
+	character(len = 80) :: form
+	character(len = 2) :: char_num(1:10)=(/"1 ", "2 ", "3 ", "4 ", "5 ", &
+					       "6 ", "7 ", "8 ", "9 ", "10"/)
 
 	! Input an integer
-	write(*,'(A)',advance='no')'Input an integer (2 - 2147483647):'
+	write(*, '(A)', advance = 'no')'Input an integer (2 - 2147483647):'
 	read(*,*)num
-	if ((num < 2).or.(2147483647 < num)) stop "The integer should be from 2 to 2147483647"
+	if ((num < 2) .or. (2147483647 < num)) stop "The integer should be from 2 to 2147483647"
 
 	! Factorization
 	composite = num
@@ -42,25 +42,25 @@ implicit none
 	! Display exponents 
 	do j = 1, index
 		if (j == 1) then
-			write(*,'(A13)', advance='no')"             "
+			write(*, '(A13)', advance = 'no')"             "
 		end if
 
 		form = "( 1X," // char_num(digit_factor(j)) // "X, I1, 3X)"
 		if (digit_exponent(j) > 1) then
 			form = "( 1X," // char_num(digit_factor(j)) // "X, I2, 2X)"
 		end if
-		write(*, form, advance='no')exponent(j)
+		write(*, form, advance = 'no')exponent(j)
 	end do
 	write(*,*)""
 
 	! Display factors 
-	write(*, '(1X, I10, A2)', advance='no')num, "= "
+	write(*, '(1X, I10, A2)', advance = 'no')num, "= "
 	do j = 1, index
 		if (j > 1) then
-			write(*, '(3X, A1)', advance='no')"*"
+			write(*, '(3X, A1)', advance = 'no')"*"
 		end if
 		form = "( 1X, I" // char_num(digit_factor(j)) // ")"
-		write(*, form, advance='no')factor(j)
+		write(*, form, advance = 'no')factor(j)
 	end do 
 	write(*,*)""
 
