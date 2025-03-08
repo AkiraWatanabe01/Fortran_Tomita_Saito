02580d76a33c371cee27ff3d07ba62d4cef03f0a
Akira Watanabe Env <72104330+AkiraWatanabe01@users.noreply.github.com>
2025-03-08 13:53:29 +0900

    Update 8text3.f90

--- Chapter8/8text3.f90
+++ Chapter8/8text3.f90
@@ -1,16 +1,16 @@
 ! Page 140 8-3
 ! 8text3.f90
-! The latest version (2024/10/20)
+! The latest version (2025/3/8)
 
 ! Subroutine for Runge Kutta method
 subroutine rungekutta (x, y, dy)
-implicit none
 
-        real(8)::d1, d2, d3, d4
-        real(8)::f
-        real(8), parameter::h = 1.0d-3
-        real(8), intent(in)::x, y
-        real(8), intent(out)::dy
+        implicit none
+        real(8) :: d1, d2, d3, d4
+        real(8) :: f
+        real(8), parameter :: h = 1.0d-3
+        real(8), intent(in) :: x, y
+        real(8), intent(out) :: dy
 
         d1 = f(x, y)
         d2 = f(x + h / 2.0d0, y + h * d1 / 2.0d0)
@@ -23,10 +23,10 @@ end subroutine rungekutta
 
 ! Function  ! f(x,y)=-x/y
 function f(x, y) result (z)
-implicit none
 
-        real(8),intent(in)::x, y
-        real(8)::z
+        implicit none
+        real(8), intent(in) :: x, y
+        real(8) :: z
 
         z = - x / y
 
@@ -34,11 +34,11 @@ end function f
 
 ! Main 
 program practice
-implicit none
 
-        integer::i
-        real(8)::x, y, dy
-        real, parameter::dx = 1.0d-3
+        implicit none
+        integer :: i
+        real(8) :: x, y, dy
+        real(8), parameter :: dx = 1.0d-3
 
         write(*, '(A)', advance = 'no')"input x :"
         read(*,*)x
@@ -51,7 +51,7 @@ implicit none
                 call rungekutta(x, y, dy)
                 x = x + dx
                 y = y + dy
-                write(*, '(F10.6,1X,F10.6)')x, y
+                write(*, '(F10.6, 1X, F10.6)')x, y
         end do
 
 end program practice
